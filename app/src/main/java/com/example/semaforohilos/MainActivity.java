ef397c88605b5b49eb5c317f2437e4928daef922
Pizzatoev <evatoronogales@gmail.com>
2025-08-28 02:04:16 -0400

    Fase 3: Añadir otro semaforo que repita las acciones de la fase 1

--- app/src/main/java/com/example/semaforohilos/MainActivity.java
+++ app/src/main/java/com/example/semaforohilos/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     Button btnIniciar;
     ImageView imgLuz;
+    ImageView imgLuz2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         btnIniciar = findViewById(R.id.btnIniciar);
         imgLuz = findViewById(R.id.imgLuz);
+        imgLuz2 = findViewById(R.id.imgLuz2);
 
         btnIniciar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,6 +72,40 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
                 thread.start();
+
+                // Segundo Semaforo
+                Thread thread2 = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (true) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    imgLuz2.setImageResource(R.drawable.luz_roja);
+                                }
+                            });
+                            try {
+                                Thread.sleep(5000);
+                            }
+                            catch (InterruptedException e){
+                                e.printStackTrace();
+                            }
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    imgLuz2.setImageResource(R.drawable.luz_off);
+                                }
+                            });
+                            try {
+                                Thread.sleep(5000);
+                            }
+                            catch (InterruptedException e){
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+                thread2.start();
             }
         });
 
